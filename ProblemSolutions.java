5fe68065fdf516e424a5c6356a1d121db3852902
MOHAMEDELJERARY <132018942+MOHAMEDELJERARY@users.noreply.github.com>
2024-11-07 04:56:04 -0600

    Add files via upload

---
+++ ProblemSolutions.java
@@ -0,0 +1,105 @@
+/******************************************************************
+ *
+ *   YOUR NAME / SECTION NUMBER
+ *  Mohamed Eljerary / 002
+ *   This java file contains the problem solutions of isSubSet, findKthLargest,
+ *   and sort2Arrays methods. You should utilize the Java Collection Framework for
+ *   these methods.
+ *
+ ********************************************************************/
+
+import java.util.*;
+
+class ProblemSolutions {
+
+    /**
+     * Method: isSubset()
+     *
+     * Given two arrays of integers, A and B, return whether
+     * array B is a subset if array A. Example:
+     *      Input: [1,50,55,80,90], [55,90]
+     *      Output: true
+     *      Input: [1,50,55,80,90], [55,90, 99]
+     *      Output: false
+     *
+     * The solution time complexity must NOT be worse than O(n).
+     * For the solution, use a Hash Table.
+     *
+     * @param list1 - Input array A
+     * @param list2 - input array B
+     * @return      - returns boolean value B is a subset of A.
+     */
+
+     public boolean isSubset(int arr1[], int arr2[]) {
+
+
+        // ADD YOU CODE HERE -- DON'T FORGET TO ADD YOR NAME AT TOP OF FILE
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for (int num : arr1) {
+            map.put(num, true);
+        }
+        for (int num : arr2) {
+            if (!map.containsKey(num)) {
+                return false;
+            }
+        }
+        return true;
+    }
+ 
+
+
+    /**
+     * Method: findKthLargest
+     *
+     * Given an Array A and integer K, return the k-th maximum element in the array.
+     * Example:
+     *      Input: [1,7,3,10,34,5,8], 4
+     *      Output: 7
+     *
+     * @param array - Array of integers
+     * @param k     - the kth maximum element
+     * @return      - the value in the array which is the kth maximum value
+     */
+
+     public int findKthLargest(int[] nums, int k) {
+
+
+        // ADD YOUR CODE HERE
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+        return minHeap.peek();
+    }
+ 
+
+
+    /**
+     * Method: sort2Arrays
+     *
+     * Given two arrays A and B with n and m integers respectively, return
+     * a single array of all the elements in A and B in sorted order. Example:
+     *      Input: [4,1,5], [3,2]
+     *      Output: 1 2 3 4 5
+     *
+     * @param array1    - Input array 1
+     * @param array2    - Input array 2
+     * @return          - Sorted array with all elements in A and B.
+     */
+
+     public int[] sort2Arrays(int[] arr1, int[] arr2) {
+
+
+        // ADD YOU CODE HERE
+        int[] merged = new int[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, merged, 0, arr1.length);
+        System.arraycopy(arr2, 0, merged, arr1.length, arr2.length);
+        Arrays.sort(merged);
+        return merged;
+    }
+ 
+
+}
